58b39a1514c4aa0c0b79c5c8b8a1a0de50f4afba
Marc Patton <pattonmj8503@gmail.com>
2021-05-29 00:24:05 -0600

    Cleanup

--- pylite/pylite.py
+++ pylite/pylite.py
@@ -3,27 +3,14 @@
 import sys
 import sqlite3
 
-#from prompt_toolkit import PromptSession
-#from prompt_toolkit.lexers import PygmentsLexer
-#from prompt_toolkit.styles import Style
-#from pygments.lexers.sql import SqlLexer
-
 from .session import PylitePromptSession
 from .commands import handle_dot_command
 from .output import print_result
 
 
 def main(database):
-    #style = Style.from_dict({
-        #"pygments.keyword": "#33C3FF",
-        #"pygments.literal.string": "#FF5833",
-    #})
     connection = sqlite3.connect(database)
     session = PylitePromptSession()
-    #session = PromptSession(
-        #lexer=PygmentsLexer(SqlLexer), 
-        #style=style, 
-        #include_default_pygments_style=False)
 
     while True:
         try:
