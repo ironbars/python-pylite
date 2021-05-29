6d7ebe2ecb49aa749bb6a12ad32cca47e3d4acd3
Marc Patton <pattonmj8503@gmail.com>
2021-05-29 13:45:22 -0600

    Add `.prompt` command
    This allows changing the default prompt

--- pylite/commands.py
+++ pylite/commands.py
@@ -1,16 +1,18 @@
+import shlex
+
 from .output import print_result
 from .input import PyliteSqlFileReader, PyliteSqlReaderError
 
 COMMANDS = dict()
 
 
-def handle_dot_command(text, connection):
-    tokens = text.split()
+def handle_dot_command(text, session):
+    tokens = shlex.split(text)
     command = tokens[0]
     cmd_args = tokens[1:]
 
     try:
-        COMMANDS[command](cmd_args, connection)
+        COMMANDS[command](cmd_args, session)
     except KeyError:
         print("Unrecognized command: {}".format(command))
 
@@ -27,18 +29,18 @@ def cmd(name):
 # continue the main REPL
 
 @cmd(".quit")
-def _quit(cmd_args, connection):
+def _quit(cmd_args, session):
     raise EOFError
 
 
 @cmd(".read")
-def _read(cmd_args, connection):
+def _read(cmd_args, session):
     sql_file = cmd_args[0]
     reader = PyliteSqlFileReader(sql_file)
 
     try:
         for sql in reader:
-            result = connection.execute(sql)
+            result = session.connection.execute(sql)
 
             print_result(result)
     except PyliteSqlReaderError:
@@ -48,15 +50,15 @@ def _read(cmd_args, connection):
 
 
 @cmd(".schema")
-def _schema(cmd_args, connection):
+def _schema(cmd_args, session):
     sql = "SELECT sql FROM sqlite_master WHERE type = 'table'"
     table = cmd_args[0] if len(cmd_args) > 0 else None
 
     if table is not None:
         sql += " AND name = ?"
-        result = connection.execute(sql, (table,))
+        result = session.connection.execute(sql, (table,))
     else:
-        result = connection.execute(sql)
+        result = session.connection.execute(sql)
 
     print(result.fetchone()[0] + ";")
     
@@ -64,18 +66,36 @@ def _schema(cmd_args, connection):
 
 
 @cmd(".tables")
-def _tables(cmd_args, connection):
+def _tables(cmd_args, session):
     sql = "SELECT name FROM sqlite_master WHERE type = 'table'"
     table = cmd_args[0] if len(cmd_args) > 0 else None
 
     if table is not None:
         sql += " AND name LIKE ?"
-        result = connection.execute(sql, (table,))
+        result = session.connection.execute(sql, (table,))
     else:
-        result = connection.execute(sql)
+        result = session.connection.execute(sql)
 
     for row in result.fetchall():
         print(row[0])
+
+    raise KeyboardInterrupt
+
+
+@cmd(".prompt")
+def _prompt(cmd_args, session):
+    new_message = cmd_args[0] if len(cmd_args) > 0 else None
+    new_continuation = cmd_args[1] if len(cmd_args) > 1 else None
+
+    if new_message is not None:
+        session.message = new_message
+    else:
+        del session.message
+
+    if new_continuation is not None:
+        session.reader.continuation = new_continuation
+    else:
+        del session.continuation
 
     raise KeyboardInterrupt
 
