243f07e05655f67d28f22985b42860ea9ff80443
Marc Patton <pattonmj8503@gmail.com>
2025-01-23 08:29:50 -0700

    Annotate `session.py`

--- src/pylite/session.py
+++ src/pylite/session.py
@@ -1,3 +1,6 @@
+from sqlite3 import Connection
+from typing import Any
+
 from prompt_toolkit import PromptSession
 from prompt_toolkit.lexers import PygmentsLexer
 from prompt_toolkit.styles import Style
@@ -8,7 +11,7 @@ from pylite.output import PyliteSqlResultWriter
 
 
 class PylitePromptSession(object):
-    def __init__(self, connection):
+    def __init__(self, connection: Connection) -> None:
         self.connection = connection
         self.style = Style.from_dict(
             {
@@ -24,44 +27,44 @@ class PylitePromptSession(object):
         self.reader = PyliteSqlPromptReader(self.session)
         self.writer = PyliteSqlResultWriter()
 
-    def prompt(self):
+    def prompt(self) -> str:
         text = self.reader.prompt()
 
         return text
 
-    def write_result(self, data, mode=None):
+    def write_result(self, data: Any, mode: str | None = None) -> None:
         self.writer.write_result(data, mode)
 
     @property
-    def message(self):
+    def message(self) -> str:
         return self.reader.message
 
     @message.setter
-    def message(self, new_message):
+    def message(self, new_message: str):
         self.reader.message = new_message
 
     @message.deleter
-    def message(self):
+    def message(self) -> None:
         self.reader.message = "pylite> "
 
     @property
-    def continuation(self):
+    def continuation(self) -> str:
         return self.reader.continuation
 
     @continuation.setter
-    def continuation(self, new_continuation):
+    def continuation(self, new_continuation: str) -> None:
         self.reader.continuation = new_continuation
 
     @continuation.deleter
-    def continuation(self):
+    def continuation(self) -> None:
         self.reader.continuation = "   ...> "
 
     @property
-    def mode(self):
+    def mode(self) -> str:
         return self.writer.mode
 
     @mode.setter
-    def mode(self, new_mode):
+    def mode(self, new_mode: str) -> None:
         self.writer.mode = new_mode
 
     @mode.deleter
@@ -69,11 +72,11 @@ class PylitePromptSession(object):
         del self.writer.mode
 
     @property
-    def dest(self):
+    def dest(self) -> str:
         return self.writer.dest
 
     @dest.setter
-    def dest(self, new_dest):
+    def dest(self, new_dest: str) -> None:
         self.writer.dest = new_dest
 
     @dest.deleter
@@ -81,17 +84,17 @@ class PylitePromptSession(object):
         del self.writer.dest
 
     @property
-    def colsep(self):
+    def colsep(self) -> str:
         return self.writer.colsep
 
     @colsep.setter
-    def colsep(self, new_colsep):
+    def colsep(self, new_colsep: str) -> None:
         self.writer.colsep = new_colsep
 
     @property
-    def rowsep(self):
+    def rowsep(self) -> str:
         return self.writer.rowsep
 
     @rowsep.setter
-    def rowsep(self, new_rowsep):
+    def rowsep(self, new_rowsep: str) -> None:
         self.writer.rowsep = new_rowsep
